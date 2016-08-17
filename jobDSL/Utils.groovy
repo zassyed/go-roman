6b323dcb796c229946333e38e177d187bff2fbb5
Thierry Lacour <thi@praqma.net>
2016-08-17 08:42:08 +0200

    Clean up the Job DSL scripts

---
+++ jobDSL/Utils.groovy
@@ -0,0 +1,37 @@
+static def configureGit(def job, def repositoryUrl) {
+    job.with {
+        scm {
+            git {
+                remote {
+                    name('origin')
+                    url(repositoryUrl)
+                }
+                branch('master')
+                configure {
+                    it / 'extensions' << 'hudson.plugins.git.extensions.impl.PathRestriction' {
+                        'includedRegions' '''\
+                                            GoWebServer/.*\\.go
+                                            GoWebServer/.*\\.html
+                                            GoWebServer/.*\\.png
+                                            version\\.txt'''.stripIndent()
+                    }
+                }
+            }
+        }
+        triggers {
+            scm('* * * * *')
+        }
+    }
+}
+
+static def configureEnv(def job, def username) {
+    job.with {
+        properties {
+            environmentVariables {
+                keepSystemVariables(true)
+                keepBuildVariables(true)
+                env('GITHUB_USERNAME', username)
+            }
+        }
+    }
+}
--- jobDSL/all_jobs.groovy
+++ jobDSL/all_jobs.groovy
@@ -1,193 +1,152 @@
-// Your github username
-// It should come as a parameter to this seed job
-//GITHUB_USERNAME="githubusername"
+// Your GitHub username, pass it as a parameter to this seed job
+// GITHUB_USERNAME="john-doe"
 
-// Variable re-used in the jobs
-PROJ_NAME="webserver"
-REPO_URL="https://github.com/${GITHUB_USERNAME}/gowebserver.git"
+projectName = "webserver"
+repositoryUrl = "https://github.com/${GITHUB_USERNAME}/gowebserver.git"
 
-// Configured to use to define the build pipeline as well
-FIRST_JOB_NAME="1.build-${PROJ_NAME}_GEN"
+buildJobName = "1.build-${projectName}_GEN"
+testJobName = "2.test-${projectName}_GEN"
+releaseJobName = "3.release-${projectName}_GEN"
+viewName = "${projectName}-jobs_GEN"
+pipelineName = "${projectName}-pipeline_GEN"
 
-job("${FIRST_JOB_NAME}") {
-  logRotator( -1, 5 ,-1 ,-1 )
-  scm {
-    git {
-      remote {
-        name('origin')
-        url("${REPO_URL}")
-      }
-      branch('master')
-      configure {
-        it / 'extensions' << 'hudson.plugins.git.extensions.impl.PathRestriction' {
-          'includedRegions' '''GoWebServer/.*\\.go
-GoWebServer/.*\\.html
-GoWebServer/.*\\.png
-version\\.txt'''
+job(buildJobName) {
+    logRotator(-1, 5, -1, -1)
+    Utils.configureGit(it, "${repositoryUrl}")
+    Utils.configureEnv(it, "${GITHUB_USERNAME}")
+    steps {
+        shell('''\
+            echo "version=\$(cat version.txt)" > props.env
+            sudo docker build --no-cache -t ${GITHUB_USERNAME}/http-app:snapshot .
+            imageid=$(sudo docker images | grep ${GITHUB_USERNAME}/http-app | grep snapshot | awk '{print $3}')
+            cid=$(sudo docker ps --filter="name=testing-app" -q -a)
+            if [ ! -z "$cid" ]
+            then
+                sudo docker rm -f testing-app
+            fi
+
+            cid=$(sudo docker run -d --name testing-app -p 8001:8000 ${GITHUB_USERNAME}/http-app:snapshot)
+            echo "cid=$cid" >> props.env
+            echo "IMAGEID=$imageid" >> props.env
+            cat props.env
+            cip=$(sudo docker inspect --format '{{ .NetworkSettings.IPAddress }}' ${cid})
+            sudo docker run --rm rufus/siege-engine -g http://$cip:8000/
+            [ $? -ne 0 ] && exit 1
+            sudo docker kill ${cid}
+            sudo docker rm ${cid}'''.stripIndent())
+    }
+    publishers {
+        downstreamParameterized {
+            trigger(testJobName) {
+                condition('SUCCESS')
+                parameters {
+                    predefinedProp('GITHUB_USERNAME', '${GITHUB_USERNAME}')
+                    gitRevision(false)
+                    propertiesFile('props.env', failTriggerOnMissing = true)
+                }
+            }
         }
-      }
     }
-  }
-  properties {
-    environmentVariables {
-      keepSystemVariables(true)
-      keepBuildVariables(true)
-      env('GITHUB_USERNAME', "${GITHUB_USERNAME}")
+}
+
+job(testJobName) {
+    logRotator(-1, 40, -1, -1)
+    parameters {
+        stringParam('GITHUB_USERNAME', '', 'GITHUB_USERNAME')
+        stringParam('version', '', 'version of the application')
+        stringParam('IMAGEID', '', 'The docker image to test')
+        stringParam('cid', '', 'The container ID')
     }
-  }
-  triggers {
-    scm('* * * * *')
-  }
-  steps{
-    shell('''
-echo "version=\$(cat version.txt)" > props.env
-
-sudo docker build --no-cache -t ${GITHUB_USERNAME}/http-app:snapshot .
-
-imageid=$(sudo docker images | grep ${GITHUB_USERNAME}/http-app | grep snapshot | awk '{print $3}')
-
-cid=$(sudo docker ps --filter="name=testing-app" -q -a)
-if [ ! -z "$cid" ]
-then
-sudo docker rm -f testing-app
-fi
-
-cid=$(sudo docker run -d --name testing-app -p 8001:8000 ${GITHUB_USERNAME}/http-app:snapshot)
-echo "cid=$cid" >> props.env
-echo "IMAGEID=$imageid" >> props.env
-cat props.env
-cip=$(sudo docker inspect --format '{{ .NetworkSettings.IPAddress }}' ${cid})
-sudo docker run --rm rufus/siege-engine -g http://$cip:8000/
-[ $? -ne 0 ] && exit 1
-sudo docker kill ${cid}
-sudo docker rm ${cid}''')
-  }
-  publishers {
-    downstreamParameterized {
-      trigger("2.test-${PROJ_NAME}_GEN") {
-        condition('SUCCESS')
-        parameters{
-          predefinedProp('GITHUB_USERNAME', '${GITHUB_USERNAME}')
-          gitRevision(false)
-          propertiesFile('props.env', failTriggerOnMissing = true)
+    steps {
+        shell('''\
+                cid=$(sudo docker ps --filter="name=testing-app" -q -a)
+                if [ ! -z "$cid" ]
+                then
+                    sudo docker rm -f testing-app
+                fi
+                testing_cid=$(sudo docker run -d --name testing-app -p 8000:8000  $IMAGEID)
+                echo "testing_cid=$testing_cid" > props.env'''.stripIndent())
+        environmentVariables {
+            propertiesFile('props.env')
         }
-      }
+        shell('''\
+                cip=$(sudo docker inspect --format '{{ .NetworkSettings.IPAddress }}' ${testing_cid})
+                sudo docker run --rm rufus/siege-engine  -b -t60S http://$cip:8000/ > output 2>&1'''.stripIndent())
+        shell('''\
+                avail=$(cat output | grep Availability | awk '{print $2}')
+                echo $avail
+                # shell uses = to compare strings, bash ==
+                if [ "$avail" = "100.00" ]
+                then
+	                echo "Availability high enough"
+	                sudo docker tag -f $IMAGEID ${GITHUB_USERNAME}/http-app:stable
+	                exit 0
+                else
+	                echo "Availability too low"
+	                exit 1
+                fi'''.stripIndent())
+
     }
-  }
+    publishers {
+        downstreamParameterized {
+            trigger(releaseJobName) {
+                condition('SUCCESS')
+                parameters {
+                    predefinedProp('VERSION', '${version}')
+                    predefinedProp('GITHUB_USERNAME', '${GITHUB_USERNAME}')
+                }
+            }
+        }
+    }
 }
 
-
-
-job("2.test-${PROJ_NAME}_GEN") {
-  logRotator( -1, 40 ,-1 ,-1 )
+job(releaseJobName) {
+    logRotator(-1, 5, -1, -1)
     parameters {
-      stringParam('GITHUB_USERNAME', '', 'GITHUB_USERNAME')
-      stringParam('version', '', 'version of the application')
-      stringParam('IMAGEID', '', 'The docker image to test')
-      stringParam('cid', '', 'The container ID')
+        stringParam('GITHUB_USERNAME', '', 'GITHUB_USERNAME')
+        stringParam('VERSION', '', 'version of the application')
     }
-  steps {
-    shell('''
-cid=$(sudo docker ps --filter="name=testing-app" -q -a)
-if [ ! -z "$cid" ]
-then
-sudo docker rm -f testing-app
-fi
-testing_cid=$(sudo docker run -d --name testing-app -p 8000:8000  $IMAGEID)
-echo "testing_cid=$testing_cid" > props.env
-''')
-    environmentVariables {
-      propertiesFile('props.env')
-    }
-    shell('''
-cip=$(sudo docker inspect --format '{{ .NetworkSettings.IPAddress }}' ${testing_cid})
-sudo docker run --rm rufus/siege-engine  -b -t60S http://$cip:8000/ > output 2>&1
-''')
-    shell('''
-avail=$(cat output | grep Availability | awk '{print $2}')
-echo $avail
-# shell uses = to compare strings, bash ==
-if [ "$avail" = "100.00" ]
-then
-	echo "Availability high enough"
-	sudo docker tag -f $IMAGEID ${GITHUB_USERNAME}/http-app:stable
-	exit 0
-else
-	echo "Availability too low"
-	exit 1
-fi
-''')
-
-  }
-  publishers {
-    downstreamParameterized {
-      trigger("3.release-${PROJ_NAME}_GEN") {
-        condition('SUCCESS')
-        parameters{
-          predefinedProp('VERSION', '${version}')
-          predefinedProp('GITHUB_USERNAME', '${GITHUB_USERNAME}')        }
-      }
-    }
-  }
-}
-
-
-
-job("3.release-${PROJ_NAME}_GEN") {
-  logRotator( -1, 5 ,-1 ,-1 )
-    parameters {
-      stringParam('GITHUB_USERNAME', '', 'GITHUB_USERNAME')
-      stringParam('VERSION', '', 'version of the application')
-    }
-  steps {
-    shell('''
-sudo docker tag -f ${GITHUB_USERNAME}/http-app:stable ${GITHUB_USERNAME}/http-app:latest
-sudo docker tag -f ${GITHUB_USERNAME}/http-app:stable ${GITHUB_USERNAME}/http-app:$VERSION
-# no git here yet
-# sudo docker tag http-app/http-app:$(git describe)
-cid=$(sudo docker ps --filter="name=deploy-app" -q -a)
-if [ ! -z "$cid" ]
-then
-sudo docker rm -f deploy-app
-fi
-sudo docker run -d --name deploy-app -p 9999:8000 ${GITHUB_USERNAME}/http-app:latest
-''')
-    shell('''
-sudo docker ps |grep ${GITHUB_USERNAME}/http-app
-sudo docker images |grep ${GITHUB_USERNAME}/http-app
-''')
-  }
-}
-
-
-
-
-
-listView("${PROJ_NAME}-jobs_GEN") {
-  description("All ${PROJ_NAME} project related jobs")
-  jobs {
-    regex(".*-${PROJ_NAME}.*")
-  }
-  columns {
-    status()
-    weather()
-    name()
-    lastSuccess()
-    lastFailure()
-    lastDuration()
-    buildButton()
+    steps {
+        shell('''\
+                sudo docker tag -f ${GITHUB_USERNAME}/http-app:stable ${GITHUB_USERNAME}/http-app:latest
+                sudo docker tag -f ${GITHUB_USERNAME}/http-app:stable ${GITHUB_USERNAME}/http-app:$VERSION
+                # no git here yet
+                # sudo docker tag http-app/http-app:$(git describe)
+                cid=$(sudo docker ps --filter="name=deploy-app" -q -a)
+                if [ ! -z "$cid" ]
+                then
+                    sudo docker rm -f deploy-app
+                fi
+                sudo docker run -d --name deploy-app -p 9999:8000 ${GITHUB_USERNAME}/http-app:latest'''.stripIndent())
+        shell('''\
+                sudo docker ps |grep ${GITHUB_USERNAME}/http-app
+                sudo docker images |grep ${GITHUB_USERNAME}/http-app'''.stripIndent())
     }
 }
 
+listView(viewName) {
+    description("All ${projectName} project related jobs")
+    jobs {
+        regex(".*-${projectName}.*")
+    }
+    columns {
+        status()
+        weather()
+        name()
+        lastSuccess()
+        lastFailure()
+        lastDuration()
+        buildButton()
+    }
+}
 
-
-buildPipelineView("${PROJ_NAME}-pipeline_GEN") {
-  title("Project ${PROJ_NAME} CI Pipeline")
-  displayedBuilds(50)
-  selectedJob("${FIRST_JOB_NAME}")
-  alwaysAllowManualTrigger()
-  showPipelineParametersInHeaders()
-  showPipelineParameters()
-  showPipelineDefinitionHeader()
-  refreshFrequency(60)
+buildPipelineView(pipelineName) {
+    title("Project ${projectName} CI Pipeline")
+    displayedBuilds(50)
+    selectedJob("${buildJobName}")
+    alwaysAllowManualTrigger()
+    showPipelineParametersInHeaders()
+    showPipelineParameters()
+    showPipelineDefinitionHeader()
+    refreshFrequency(60)
 }
